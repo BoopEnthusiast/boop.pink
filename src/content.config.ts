782b9fe1882db292bd2a1dfa45b3863b31434a6a
Boop <102631339+BoopEnthusiast@users.noreply.github.com>
2025-03-09 16:51:28 +1300

    deleting failed markdown css attempt

--- src/content.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineCollection } from 'astro:content';
-
-import { glob } from 'astro/loaders';
-
-const dailyBlog = defineCollection({
-    loader: glob({ pattern: "**/*.md", base: "./src/pages/daily" }),
-});
-
-export const collections = { dailyBlog };
