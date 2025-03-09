3bcfb9d198ae042095a6cc01f431720b34b7b779
Boop <102631339+BoopEnthusiast@users.noreply.github.com>
2025-03-10 06:51:38 +1300

    Add footer to other websites and author frontmatter

--- markdown-stylization.mjs
+++ markdown-stylization.mjs
@@ -1,5 +1,6 @@
 export function markdownStylization() {
     return function (tree, file) {
         file.data.astro.frontmatter.layout = '/src/layouts/MarkdownLayout.astro';
+        file.data.astro.frontmatter.author = 'Boop'
     }
 }
