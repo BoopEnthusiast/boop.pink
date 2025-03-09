c2374b77a612dcf1895349cd5c3d6722b0e20b45
Boop <102631339+BoopEnthusiast@users.noreply.github.com>
2025-03-09 17:59:37 +1300

    laying out markdown nicer

--- markdown-stylization.mjs
+++ markdown-stylization.mjs
@@ -1,5 +1,4 @@
 export function markdownStylization() {
-    // All remark and rehype plugins return a separate function
     return function (tree, file) {
         file.data.astro.frontmatter.layout = '/src/layouts/MarkdownLayout.astro';
     }
